83b8fb97703762178f62c3bbe5ec6463999ca20a
David B <88885429+crux888@users.noreply.github.com>
2021-12-11 11:57:58 +0000

    Create coloring.py

---
+++ part-1-the-basics/coloring.py
@@ -0,0 +1,18 @@
+from colored_grid import ColoredGrid
+from binary_tree import BinaryTree
+from sidewinder import Sidewinder
+from aldous_broder import AldousBroder
+from wilsons import Wilsons
+from hunt_and_kill import HuntAndKill
+from recursive_backtracker import RecursiveBacktracker
+
+
+grid = ColoredGrid(20, 20)
+algorithm = RecursiveBacktracker()
+algorithm.on(grid)
+
+start = grid[grid.rows // 2, grid.columns // 2]
+grid.distances = start.distances()
+
+grid.to_s()
+grid.to_png()
